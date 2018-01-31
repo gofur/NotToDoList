8ac63ea7c69393cda99b79612c760c40127dfe04
gofur <gofurbrente01@gmail.com>
2018-02-01 06:54:55 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 //first import
-import React, {Component} from 'react';
-
+import React, {Component} from 'react'
+import {View,Text} from 'react-native'
 
 //create class
-class App extends Component {
+export default class App extends Component {
   render(){
-    
+    return (
+      <View style={{flex:1, alignSelf:'center', justifyContent:'center'}}>
+      <Text>Hello Inahost.Net</Text>
+      </View>
+    )
   }
 }
