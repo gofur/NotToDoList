2b8eda93c31fa7628269c0b5fec4c26c1b19918c
gofur <gofurbrente01@gmail.com>
2018-02-07 06:05:47 +0700

    List Item Component

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import {
   Left, Right, CheckBox
 } from 'native-base';
 
+
+import TodoItem from './components/TodoItem';
+
 export default class App extends Component{
 
   todos = [
@@ -34,21 +37,7 @@ export default class App extends Component{
 
         <Content>
           <List>
-
-
-            {this.todos.map((todo)=> (
-              <ListItem key={todo.id}>
-                <Left>
-                  <CheckBox checked={false} />
-                </Left>
-                <Body>
-                  <Text>{todo.todo}</Text>
-                </Body>
-                <Right/>
-              </ListItem>
-            ))}
-
-
+            {this.todos.map((todo)=> <TodoItem todo={todo} key={todo.id}/>)}
           </List>
         </Content>
       </Container>
