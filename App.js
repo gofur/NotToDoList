85395f42a4f28cfc5edcbf3ade78922052a4e38f
gofur <gofur@gofurs-iMac.local>
2018-03-01 17:13:25 +0700

    update data

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {
   Container, Content, Text,
   Header, Body, List, ListItem,
-  Left, Right, CheckBox
+  Left, Right, CheckBox, Fab, Icon,
 } from 'native-base';
 import {FlatList} from 'react-native';
 
@@ -11,42 +11,65 @@ import TodoItem from './components/TodoItem';
 
 export default class App extends Component{
 
-  todos = [
-    {
-      id: "1",
-      todo: "First Not To Do"
-    },
-    {
-      id: "2",
-      todo: "Second Not To Do"
-    },
-    {
-      id: "3",
-      todo: "Third Not To Do"
-    }
-  ];
+  constructor(){
+    super();
+    this.state =  {
+      count: 0,
+      todos: []
+    };
+  }
+  
+//ini akan dijalankan sebelum render
+  componentWillMount(){
+    console.log('componentWillMount');
+  }
+
+//ini akan dijalankan setelah render
+  componentDidMount(){
+    console.log('componentDidMount');
+    this.handleIncTodo();
+  }
+
+
+
 
   _keyExtractor = (item, index) => item.id;
+
+  handleIncTodo(){
+    let count = this.state.count + 1;
+    let todos = this.state.todos;
+    todos.push({
+      id: count,
+      todo: "Not To Do #" + count
+    })
+    this.setState({
+      todo: todos,
+      count: count
+    });
+  }
 
   render(){
     return(
       <Container>
         <Header>
           <Body>
-            <Text>Not To Do List</Text>
+            <Text>Not To Do List ({this.state.count})</Text>
           </Body>
         </Header>
 
         <Content>
           <List>
-            <FlatList
-              data={this.todos}
-              keyExtractor={this._keyExtractor}
-              renderItem={({item}) => <TodoItem todo={item}/>}
-            />
-             {/*this.todos.map((todo)=> <TodoItem todo={todo} key={todo.id}/>)*/}
+
+             {this.state.todos.map((todo)=> <TodoItem todo={todo} key={todo.id}/>)}
           </List>
         </Content>
+
+        <Fab
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => this.handleIncTodo()}>
+            <Icon name="add" />
+          </Fab>
       </Container>
     );
 
